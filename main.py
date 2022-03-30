9f6e0a1b55f00267be2a29c1ce576cf2ba4fb23d
Himanshu Bhoir <himanshubhoir25@gmail.com>
2022-03-30 21:44:21 +0530

    working on

--- main.py
+++ main.py
@@ -22,14 +22,17 @@ net.setInputSwapRB(True)
 
 while True:
     success,img = cap.read()
-    classIds,confs,bbox = net.detect(img, confThreshold=0.5)
+
+    classIds,confs,bbox = net.detect(img, confThreshold=0.6)
     print(classIds, bbox)
 
     if len(classIds) != 0:
         for classId, confidence, box in zip(classIds.flatten(), confs.flatten(), bbox):
             cv2.rectangle(img, box, color = (0, 255, 0), thickness=2)
-            cv2.putText(img, classNames[classId-1].upper(), (box[0]+10, box[1]+30),cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
-            cv2.putText(img, str(confidence),(box[0]+150, box[1]+30),cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+            cv2.putText(img, classNames[classId-1].upper(), (box[0]+10, box[1]+20),cv2.FONT_HERSHEY_COMPLEX,0.5,(0, 255, 0),2)
+            cv2.putText(img, str(confidence),(box[0]+100, box[1]+20),cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 255, 0), 2)
+
+
 
     cv2.imshow('output', img)
     cv2.waitKey(1)
